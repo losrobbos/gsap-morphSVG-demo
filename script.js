c4da625a55cc376379cb7906851688703aedc8dc
losrobbos <robert.ristock@googlemail.com>
2025-05-27 16:19:52 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+import gsap from "gsap"
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin"
+
+gsap.registerPlugin(MorphSVGPlugin)
+
+// morph on start
+const morph = gsap.to("#circle", {
+  duration: 1,
+  // morph circle into this other svg (=> using the two d paths of the shapes)
+  morphSVG: "#hippo",
+  repeat: 1,
+  yoyo: true,
+  repeatDelay: 0.2,
+});
+
+document.getElementById("play").onclick = function () {
+  morph.restart(true);
+};
